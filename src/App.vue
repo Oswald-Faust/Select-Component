a500d6652dc12fe4036ebac6f8a4d8141aa4b557
faust.troukou@epitech.eu <ofaust>
2023-08-21 15:03:31 +0100

    Debute component

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+  a
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
